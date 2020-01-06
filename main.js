cb70e028d0e81d6ef5a7cd52f6d0aa635e469fa8
Rull Deef <34412244+RullDeef@users.noreply.github.com>
2020-01-06 14:52:37 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 import Engine from "./engine.js"
 
 console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
+console.log("main.js imported!")
